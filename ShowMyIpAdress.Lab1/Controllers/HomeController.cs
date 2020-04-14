43b578f75a2f7d6ca11588c7108cd16b7fe04415
47720480 <david.n.stahl@gmail.com>
2020-04-14 11:13:30 +0200

    first commit

---
+++ ShowMyIpAdress.Lab1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShowMyIpAdress.Lab1.Models;
+
+namespace ShowMyIpAdress.Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
